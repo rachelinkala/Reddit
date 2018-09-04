8ee2008825848fc03a6fb8e784147dd3be323dee
Rachel Inkala <rachelinkala@gmail.com>
2018-09-04 10:27:27 -0600

    sub model, controller, and routes generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "subs#index"
+
+  resources :subs, only: [:new, :create, :edit, :update]
+  
+  get "/subs", to: "subs#index"
+  get "/subs/:id" to: "subs#show"
+  delete "/subs/:id" to: "subs#destroy"
+
 end
