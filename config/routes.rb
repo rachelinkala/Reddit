eb2bacbb6323cc6849ca41ca21d3bd2dbb1843f7
Rachel Inkala <rachelinkala@gmail.com>
2018-09-04 14:24:29 -0600

    added sub topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root "subs#index"
 
-  resources :subs, only: [:new, :create, :edit, :update]
-  
-  get "/subs", to: "subs#index"
-  get "/subs/:id" to: "subs#show"
-  delete "/subs/:id" to: "subs#destroy"
+  resources :subs do
+    resources :topics
+  end
 
 end
